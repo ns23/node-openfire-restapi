c1ad27c075f90ec97082f8f488e4c4915342d7d0
Nitesh Sawant <ns23@users.noreply.github.com>
2020-08-30 19:13:35 +0530

    Updated user related endpoints

--- lib/class/User.ts
+++ lib/class/User.ts
@@ -4,55 +4,144 @@ import { IParamUser, IRetriveUserSearch, IRetriveUsersResponse, IUser } from './
 import { RosterObject } from '../interfaces/Roster';
 import { Response } from 'got/dist/source';
 
-
 class User {
-    private endPoint = 'users';
-    constructor(private rest: Rest) { }
+  private endPoint = 'users';
+  constructor(private rest: Rest) {}
 
-    async retriveUsers(query: IRetriveUserSearch): Promise<IRetriveUsersResponse> {
-        const searchParams = new URLSearchParams(query as any);
-        const users = (await this.rest.get(this.endPoint, { searchParams: searchParams.toString() })) as IRetriveUsersResponse;
-        return users;
-    }
-    async retriveUser(username: string): Promise<IUser> {
-        const endPoint = `users/${username}`;
-        const user = await this.rest.get(endPoint) as IUser;
-        return user;
-    }
+  /**
+   * Retrieve users
+   * @description Endpoint to get all or filtered users
+   */
+  async retriveUsers(query: IRetriveUserSearch): Promise<IRetriveUsersResponse> {
+    const searchParams = new URLSearchParams(query as any);
+    const users = (await this.rest.get(this.endPoint, {
+      searchParams: searchParams.toString(),
+    })) as IRetriveUsersResponse;
+    return users;
+  }
 
-    async createUser(data: IParamUser): Promise<{ statusMessage: string | undefined; statusCode: number }> {
-        const endPoint = 'users';
-        const { statusCode, statusMessage } = await this.rest.post(endPoint, { json: data }) as Response;
-        return {
-            statusCode,
-            statusMessage,
-        };
-    }
+  /**
+   * Retrieve a user
+   * @description Endpoint to get information over a specific user
+   */
+  async retriveUser(username: string): Promise<IUser> {
+    const url = `users/${username}`;
+    const user = (await this.rest.get(url)) as IUser;
+    return user;
+  }
 
-    async deleteUser(username: string): Promise<{ body: object; statusCode: number }> {
-        const endPoint = `users/${username}`;
-        const { body, statusCode } = await this.rest.delete(endPoint) as Response;
-        return {
-            body: body as object,
-            statusCode,
-        };
-    }
+  /**
+   * Create a user
+   * @description Endpoint to create a new user
+   */
+  async createUser(
+    data: IParamUser,
+  ): Promise<{ statusMessage: string | undefined; statusCode: number }> {
+    const { statusCode, statusMessage } = await this.rest.post(this.endPoint, {
+      json: data,
+    });
+    return {
+      statusCode,
+      statusMessage,
+    };
+  }
 
-    async updateUser(username: string, data: IParamUser): Promise<IUser> {
-        const endPoint = `users/${username}`;
-        const response = await this.rest.put(endPoint, { json: data });
-        const user = response.body as IUser;
-        return user;
-    }
+  /**
+   * Delete a user
+   * @description Endpoint to delete a user
+   */
+  async deleteUser(username: string): Promise<{ body: object; statusCode: number }> {
+    const endPoint = `users/${username}`;
+    const { body, statusCode } = (await this.rest.delete(endPoint)) as Response;
+    return {
+      body: body as object,
+      statusCode,
+    };
+  }
 
-    async retriveUserRoster(username: string): Promise<RosterObject> {
-        const endPoint = `users/${username}/roster`;
-        const rosters = await this.rest.get(endPoint) as RosterObject;
-        return rosters;
-    }
+  /**
+   * Update a user
+   * @description Endpoint to update / rename a user
+   */
+  async updateUser(username: string, data: IParamUser): Promise<IUser> {
+    const endPoint = `users/${username}`;
+    const response = await this.rest.put(endPoint, { json: data });
+    const user = response.body as IUser;
+    return user;
+  }
 
+  /**
+   * Retrieve all user groups
+   * TODO : Add return type
+   * @description Endpoint to get group names of a specific user
+   */
+  async getUserGroups(username: string): Promise<Object> {
+    const url = `${this.endPoint}/users/${username}/groups`;
+    const groups = await this.rest.get(url);
+    return groups;
+  }
 
+  /**
+   * Add user to group
+   * @description Endpoint to add user to a group
+   */
+  async addUserToGroup(username: string, groupname: string): Promise<number> {
+    const url = `${this.endPoint}/${username}/groups/${groupname}`;
+    const { statusCode } = await this.rest.post(url);
+    return statusCode;
+  }
+
+  /**
+   * Delete a user from a group
+   * TODO add types
+   * @description Endpoint to remove a user from a group
+   */
+  async deleteUserFromGroup(username: string, groupname: string): Promise<number> {
+    const url = `${this.endPoint}/${username}/groups/${groupname}`;
+    const { statusCode } = await this.rest.delete(url);
+    return statusCode;
+  }
+
+  /**
+   * Lockout a user
+   * @description  Endpoint to lockout / ban the user from the chat server.
+   * The user will be kicked if the user is online.
+   */
+  async lockoutUser(username: string): Promise<number> {
+    const url = `/lockouts/${username}`;
+    const { statusCode } = await this.rest.delete(url);
+    return statusCode;
+  }
+
+  async unlockUser(username: string): Promise<number> {
+    const url = `/lockouts/${username}`;
+    const { statusCode } = await this.rest.post(url);
+    return statusCode;
+  }
+
+  /**
+   * Delete a user from a groups
+   * TODO add types
+   * @description Endpoint to remove a user from a groups
+   */
+  async deleteUserFromGroups(username: string): Promise<Object> {
+    const url = `${this.endPoint}/${username}/groups`;
+    const { body, statusCode } = (await this.rest.delete(url)) as Response;
+    return {
+      body: body as object,
+      statusCode,
+    };
+  }
+
+  /**
+   * Retrieve user roster
+   * Endpoint to get roster entries (buddies) from a specific user
+   */
+  async retriveUserRoster(username: string): Promise<RosterObject> {
+    const endPoint = `users/${username}/roster`;
+    const rosters = (await this.rest.get(endPoint)) as RosterObject;
+    return rosters;
+  }
 }
-
 
 export default User;
